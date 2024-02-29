cf09b61e8f68855776a0c707b19765226edcaec2
François Klingler <francois@klingler.fr>
2024-02-29 10:44:37 +0100

    fix: fix base URL in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/mtgproxy/',
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js'
